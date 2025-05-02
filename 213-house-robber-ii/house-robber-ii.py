1e59d892f8e71c105629ac07b8dba3f1ae7e54c1
Smith L Surendran <42495076+smithSurendran@users.noreply.github.com>
2025-05-02 01:32:23 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (58.06%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,27 +1,22 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n= len(nums)
-        if n==1:
+
+        def rob_adj(nums):
+            n= len(nums)
+            dp=[0]*n
+            dp[0]=nums[0]
+            dp[1]= max(nums[0], nums[1])
+
+            for i in range(2,n):
+                dp[i]= max(dp[i-2]+nums[i], dp[i-1])
+
+            return dp[-1]   
+        if len(nums)==1:
             return nums[0]
-        if n==2:
-            return max(nums[0],nums[1])
-        dp1=[0]*(n-1)
-        dp1[0]=nums[0]
-        dp1[1]= max(dp1[0], nums[1])
-
-        for i in range(2,n-1):
-            dp1[i]= max(dp1[i-2]+nums[i], dp1[i-1])
-
-        dp2=[0]*(n-1)
-        dp2[0]=nums[1]
-        dp2[1]=max(dp2[0],nums[2])
-
-        for i in range(2, n-1):
-            dp2[i]=max(dp2[i-2]+nums[i+1], dp2[i-1])
-
-        print(dp1)
-        print(dp2)
-
-        return max(dp1[-1], dp2[-1])    
+        if len(nums)==2:
+            return max(nums[0], nums[1])
+        first_house= rob_adj(nums[:-1])
+        last_house= rob_adj(nums[1:])
+        return max(first_house, last_house)
         
         
