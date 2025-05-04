9012883164f8e8cf44e582c465af1e151627b0c0
Smith L Surendran <42495076+smithSurendran@users.noreply.github.com>
2025-05-04 11:33:03 -0400

    Time: 10 ms (5.61%) | Memory: 24.9 MB (5.09%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -8,33 +8,36 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        slow= fast= head
-        while fast and fast.next:
-            slow= slow.next
-            fast= fast.next.next
-        mid= slow
-
-        def rev(node):
-            if node is None or node.next is None:
+        def reverseList(node):
+            if not node or node.next is None:
                 return node
-            new_head= rev(node.next)
+            
+            new_head= reverseList(node.next)
 
             node.next.next= node
             node.next= None
 
             return new_head
         
-        second= rev(mid)
-        first= head
-        while second.next:
-            temp = first.next
-            first.next=second
-            first= temp
+        slow= fast= head
 
-            temp=second.next
-            second.next= first
-            second=temp
+        while fast and fast.next:
+            slow= slow.next
+            fast= fast.next.next
+        
+        mid= slow
+        revList= reverseList(mid)
 
+        curr= head
+
+        while revList.next:
+            temp= curr.next
+            curr.next= revList
+            curr= temp
+
+            temp=revList.next
+            revList.next= curr
+            revList= temp
         return head
              
 
