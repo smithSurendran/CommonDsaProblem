81849c768f79fc1232b8e3a3b771c0a1b4b38b01
Smith L Surendran <42495076+smithSurendran@users.noreply.github.com>
2025-05-05 10:53:27 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (16.17%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,22 +1,23 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        def robadj(houses):
+            n= len(houses)
 
-        def rob_adj(nums):
-            n= len(nums)
+            if n==1:
+                return houses[0]
+            if n==2:
+                return max(houses[0], houses[1])
+            
             dp=[0]*n
-            dp[0]=nums[0]
-            dp[1]= max(nums[0], nums[1])
+            dp[0]= houses[0]
+            dp[1]= max(houses[0], houses[1])
 
-            for i in range(2,n):
-                dp[i]= max(dp[i-2]+nums[i], dp[i-1])
-
-            return dp[-1]   
-        if len(nums)==1:
+            for i in range(2, n):
+                dp[i]= max(dp[i-2]+houses[i], dp[i-1])
+            
+            return dp[-1]
+        n= len(nums)
+        if n==1:
             return nums[0]
-        if len(nums)==2:
-            return max(nums[0], nums[1])
-        first_house= rob_adj(nums[:-1])
-        last_house= rob_adj(nums[1:])
-        return max(first_house, last_house)
-        
+        return max(robadj(nums[0:n-1]), robadj(nums[1:n]))
         
