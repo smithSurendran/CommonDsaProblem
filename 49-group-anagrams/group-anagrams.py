58b09db301f6a312720aa30a7aae69906d5c4d5d
Smith L Surendran <42495076+smithSurendran@users.noreply.github.com>
2025-05-08 21:59:57 -0400

    Time: 11 ms (82.96%) | Memory: 21.3 MB (43.42%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,11 +1,8 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        freq=defaultdict(list)
+        anagram=defaultdict(list)
 
-        for word in strs:
-            key =  ''.join(sorted(word))
-            freq[key].append(word)
-        
-        print(freq.values())
-
-        return list(freq.values())
+        for s in strs:
+            key =tuple(sorted(s))
+            anagram[key].append(s)
+        return list(anagram.values())
