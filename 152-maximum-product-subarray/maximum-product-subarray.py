ed6cce0d0f4437da5b89a1e56170e49a06506ee2
Smith L Surendran <42495076+smithSurendran@users.noreply.github.com>
2025-07-21 13:10:12 -0400

    Time: 5 ms (93.73%) | Memory: 18.4 MB (35.60%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -1,20 +1,17 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-
-        #kadane Algorithm
-
-        curr_max= nums[0]
-        curr_min= nums[0]
-        result= nums[0]
-
-        for i in range(1, len(nums)):
-            if nums[i]<0:
+        max_pdt=nums[0]
+        curr_max=curr_min=nums[0]
+        for num in nums[1:]:
+            if num<0:
                 curr_min, curr_max= curr_max, curr_min
-            curr_max= max(nums[i], curr_max*nums[i])
-            curr_min= min(nums[i], curr_min*nums[i])
-
-            result= max(result,curr_max)
-
-
             
-        return result        
+            curr_max= max(num, curr_max*num)
+
+            curr_min= min(num, curr_min*num)
+
+            max_pdt= max(max_pdt, curr_max)
+
+        return max_pdt
+            
+
